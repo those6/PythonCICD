1d944337991143b9d566c81031e839fad19c0c4e
Gerardo Robles <those6@gmail.com>
2023-11-26 18:13:35 -0600

    Testing unit test

--- src/hello.py
+++ src/hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, Derik"
+    return "Derik is smelly"
     
 if __name__ == "__main__":
     app.run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
 from hello import index
 
 def test_index():
-    assert index() == "Hello, Derik"
+    assert index() == "Derik is Awesome!!"
